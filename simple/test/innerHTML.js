84a67e03b4b27553018000409b62df4703063a4b
Ivan L. Galeano S <ilgaleanos@users.noreply.github.com>
2017-02-19 22:16:45 -0500

    Update innerHTML.js

--- simple/test/innerHTML.js
+++ simple/test/innerHTML.js
@@ -1,64 +1,52 @@
-const renderTable = new Fragment("#App",`
+const Fragment = require('../core/fragment.js');
+
+
+
+const renderTable = new Fragment("App",`
         <div class="Main">
-                <table class="Table"><tbody>
-                        <tr class="TableRow{{? it.active }}active{{?}}" data-id="{{= it.id }}">
-                                <td class="TableCell" data-text="#{{= it.id }}">#{{= it.id }}</td>
-                                {{~ it.props: prop}}
-                                        <td class="TableCell" data-text="#{{= prop }}">#{{= prop }}</td>
-                                {{~}}
-                        </tr>
+                <table id="Table" class="Table" ><tbody>
+                        {{~ it.items: item}}
+                                <tr class="TableRow {{? item.active }}active{{?}}" data-id="{{= item.id }}">
+                                        <td class="TableCell" data-text="#{{= item.id }}">#{{= item.id }}</td>
+                                        {{~ item.props: prop}}
+                                                <td class="TableCell" data-text="#{{= prop }}">{{= prop }}</td>
+                                        {{~}}
+                                </tr>
+                        {{~}}
                 </tbody></table>
+        </div>
         `, {})
 
-const renderAnimBox = new Fragment("#App",`
+const renderAnim = new Fragment("App",`
+<div  class="Main">
         <div class="Anim">
         {{~ it.items: item}}
-                <div class="AnimBox" style="border-radius:{{ item.time % 10 }}px; background:rgba(0,0,0,{{0.5 + (( item.time % 10) / 10) }})" data-id="{{= item.id }}"></div>
+                <div class="AnimBox" style="border-radius:{{= item.time % 10 }}px; background:rgba(0,0,0,{{= 0.5 + (( item.time % 10) / 10) }})" data-id="{{= item.id }}"></div>
         {{~}}
         <div>
+</div>
         `, {})
 
 
-function renderTreeNode(data) {
-        var result = '<ul class="TreeNode">', n;
-        for (let i = 0, k = data.children.length; i < k; i++) {
-                n = data.children[i];
-                if ( n.container ) {
-                        result = result + renderTreeNode(n) :
-                } else {
-                        result = result + '<li class="TreeLeaf">';
-                        result = result + n.props.id;
-                        result = result +'</li>';
-                        return;
-                }
-        }
-        result = result +'</ul>'
-        return result;
+function renderTreeNode(c) {
+       for (var a = '<ul class="TreeNode">', d = 0; d < c.children.length; d++) var b = c.children[d],
+           a = a + (b.container ? renderTreeNode(b) : '<li class="TreeLeaf">' + b.id + "</li>");
+       return a + "</ul>"
 }
 
-const renderTree = new Fragment("#App", `
-        <div class="Tree">
-                <ul class="TreeNode">
-                        {{~ it.children: child }}
-                                {{? child.container }}
-                                        {{ this.renderTreeNode(child); }}
-                                {{??}}
-                                        <li class="TreeLeaf">{{= it.id }}</li>
-                                {{?}}
-                        {{~}}
-                </ul>
-        </div>
-        `,{})
-
+const container = document.getElementById('App');
+function renderTree(data) {
+        container.innerHTML = '<div class="Main"><div class="Tree">' + renderTreeNode(data.root) + "</div></div>"
+}
 
 function renderMain(data) {
         const location = data && data.location;
         if (location === "table") {
-                renderTable.render(data.table);
+                renderTable.render({data: data.table});
         } else if (location === "anim") {
-                renderAnim.render(data.anim);
+                renderAnim.render({data: data.anim});
         } else if (location === "tree") {
-                renderTree.render(data.tree);
+                renderTree( data.tree);
         }
 }
 
@@ -72,15 +60,13 @@ function handleClick(e) {
         }
 }
 
+
 document.addEventListener("DOMContentLoaded", (e) => {
         uibench.run(
             (state) => {
                         renderMain(state);
                         if (state.location === "table") {
-                              const cells = container.querySelectorAll(".TableCell");
-                              for (var i = 0, k = cells.length; i < k; i++) {
-                                      cells[i].onclick = handleClick;
-                              }
+                              container.onclick = handleClick;
                         }
                 },
                 (samples) => {
